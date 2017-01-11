3f26332cdf03d8b429c4995cadfb521f61808c0b
Flennerhag <sebastianflennerhag@hotmail.com>
2017-01-11 11:30:02 +0100

    final fix for first successful build

--- mlens/__init__.py
+++ mlens/__init__.py
@@ -7,6 +7,6 @@ licence: MIT
 A library for ensemble learning
 """
 
-__version__ = "0.1.1.dev"
+__version__ = "0.1.1.dev0"
 
 __all__ = ['ensemble', 'model_selection', 'visualization', 'preprocessing']

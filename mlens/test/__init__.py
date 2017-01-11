eec75974ec90d4f0e70c0a375fca67e489902226
Flennerhag <sebastianflennerhag@hotmail.com>
2017-01-11 21:58:26 +0100

    init for test

---
+++ mlens/test/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+author: Sebastian Flennerhag
+date: 12/01/2017
+A simple script to check that ensemble runs and integrates well with sklearn.
+"""
+
+from . import test
+
+__all__ = ['test']

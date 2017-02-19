e7363f849377e683887fb1d1350654171cf86d7f
Flennerhag <sebastianflennerhag@hotmail.com>
2017-02-19 13:23:31 +0100

    checking suite

--- mlens/utils/__init__.py
+++ mlens/utils/__init__.py
@@ -8,5 +8,7 @@ date: 10/01/2017
 """
 
 from .utils import _slice, pickle_save, pickle_load, print_time, IdTrain
+from .checks import check_is_fitted, check_inputs
 
-__all__ = ['_slice', 'pickle_save', 'pickle_load', 'print_time', 'IdTrain']
+__all__ = ['_slice', 'pickle_save', 'pickle_load', 'print_time', 'IdTrain',
+           'check_is_fitted', 'check_inputs']

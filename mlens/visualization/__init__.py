2428ba29785dbb05ee8931a733af32d3e86b856d
Flennerhag <sebastianflennerhag@hotmail.com>
2017-03-08 13:39:43 +0100

    vis_import_warning

--- mlens/visualization/__init__.py
+++ mlens/visualization/__init__.py
@@ -3,11 +3,20 @@
 author: Sebastian Flennerhag
 """
 
+import warnings
+
 try:
     from .correlations import corrmat, clustered_corrmap, corr_X_y
     from .var_analysis import IO_plot_comp, IO_plot, exp_var_plot
 except ImportError:
-    pass
+    warnings.warn("Matplotlib and Seaborn not installed. Cannot load "
+                  "visualization module.", ImportWarning)
+    corrmat = None
+    clustered_corrmap = None
+    corr_X_y = None
+    IO_plot = None
+    IO_plot_comp = None
+    exp_var_plot = None
 
 __all__ = ['corrmat', 'clustered_corrmap', 'corr_X_y',
            'IO_plot_comp', 'IO_plot', 'exp_var_plot']

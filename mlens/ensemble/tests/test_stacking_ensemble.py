109a53d56c902dd63a67425ca57cce734a35310c
Flennerhag <sebastianflennerhag@hotmail.com>
2017-03-14 14:02:13 +0100

    check estimator, docstrings

--- mlens/ensemble/tests/test_stacking_ensemble.py
+++ mlens/ensemble/tests/test_stacking_ensemble.py
@@ -4,10 +4,17 @@ author: Sebastian Flennerhag
 """
 
 from __future__ import division, print_function, with_statement
+from contextlib import redirect_stderr
+import os
+
+import numpy as np
+from pandas import DataFrame
 
 from mlens.ensemble import StackingEnsemble
 from mlens.metrics import rmse
 from mlens.metrics.metrics import rmse_scoring
+from mlens.utils.estimator_checks import InitMixin
+
 from sklearn.linear_model import Lasso
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
@@ -15,8 +22,8 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.base import clone
-import numpy as np
-from pandas import DataFrame
+from sklearn.utils.estimator_checks import check_estimator
+
 import warnings
 
 # Training data
@@ -92,7 +99,33 @@ grid_ens.add({'mm': [SVR()]}, {'mm': [MinMaxScaler()]})
 grid_ens.add_meta(SVR())
 
 
+class TestStackingEnsemble(InitMixin, StackingEnsemble):
+    """Wrapper around StackingEnsemble to check estimator behavior."""
+
+    def __init__(self):
+        super(TestStackingEnsemble, self).__init__()
+
+
+def test_estimator_behavior():
+    """[StackingEnsemble] Test Scikit-learn compatibility."""
+    with warnings.catch_warnings(record=True) as w:
+        # Assert the StackingEnsemble passes the Scikit-learn estimator test.
+        check_estimator(TestStackingEnsemble)
+
+    # Check that all warnings were mlens warnings
+    if not isinstance(w, list):
+        w = [w]
+
+    for m in w:
+        filename = m.filename.lower()
+
+        assert 'mlens' in filename
+        assert 'sklearn' not in filename
+        assert 'scikit-learn' not in filename
+
+
 def test_no_preprocess_ensemble():
+    """[StackingEnsemble] Test without any preprocessing."""
     ens1.fit(X, y)
     ens2.fit(X, y)
     ens3.fit(X, y)
@@ -113,17 +146,19 @@ def test_no_preprocess_ensemble():
 
 
 def test_preprocess_ensemble():
-
+    """[StackingEnsemble] Test with preprocessing."""
     # Check that cloning and set params work
     ens = clone(ensemble).set_params(**fit_params)
 
     ensemble.set_params(**fit_params)
 
-    ens.fit(X[:900], y[:900])
-    ensemble.fit(X[:900], y[:900])
+    with open(os.devnull, 'w') as f, redirect_stderr(f):
+        # Silences the training output, but lets warnings and errors through.
+        ens.fit(X[:900], y[:900])
+        ensemble.fit(X[:900], y[:900])
 
-    score1 = rmse(ens, X[900:], y[900:])
-    score2 = rmse_scoring(y[900:], ens.predict(X[900:]))
+        score1 = rmse(ens, X[900:], y[900:])
+        score2 = rmse_scoring(y[900:], ens.predict(X[900:]))
 
     assert score1 == -score2
     assert ens.get_params()['n_jobs'] == -1
@@ -131,16 +166,20 @@ def test_preprocess_ensemble():
 
 
 def test_grid_search():
-
+    """[StackingEnsemble] Test in GridSearch."""
     grid_ens.set_params(**{'n_jobs': 1})
 
     grid = GridSearchCV(grid_ens, param_grid=grid_params,
                         cv=2, scoring=rmse, n_jobs=1)
-    grid.fit(X, y)
+
+    with open(os.devnull, 'w') as f, redirect_stderr(f):
+        # Silences the training output, but lets warnings and errors through.
+        grid.fit(X, y)
     assert str(grid.best_score_)[:16] == '-0.0739108130567'
 
 
 def test_ensemble_exception_handling():
+    """[StackingEnsemble] Test exception handling."""
     # Currently this test just ensures the ensemble runs through
     ensemble.set_params(**{  # will cause error
                            'layers__layer-1__np-rf__min_samples_leaf': 1.01,
@@ -150,7 +189,7 @@ def test_ensemble_exception_handling():
                            'layers__layer-1__np-rf__max_features': 1.01,
                            'layers__layer-1__mm-svr__C': 11.834807428701293,
                            'layers__layer-1__sc-ls__alpha':
-                               0.0014284293508642438,
+                           0.0014284293508642438,
                            'layers__layer-1__np-rf__max_depth': 4,
                            'n_jobs': -1,
                            'verbose': 0,

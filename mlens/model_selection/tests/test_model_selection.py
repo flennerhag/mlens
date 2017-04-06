5979a4587d0280a45344d2bfa5e492259ae91d1d
Flennerhag <sebastianflennerhag@hotmail.com>
2017-04-06 21:07:01 +0100

    approx equal

--- mlens/model_selection/tests/test_model_selection.py
+++ mlens/model_selection/tests/test_model_selection.py
@@ -24,7 +24,9 @@ def test_no_prep():
             estimators=[OLS()],
             param_dicts={'ols': {'offset': randint(1, 10)}},
             n_iter=10)
-    assert evl.summary['test_score_mean']['ols'] == np.abs(-28.376957869124858)
+
+    np.testing.assert_approx_equal(
+            evl.summary['test_score_mean']['ols'], 28.376957869124858)
 
 
 def test_w_prep():
@@ -40,23 +42,26 @@ def test_w_prep():
                  param_dicts={'ols': {'offset': randint(1, 10)}},
                  n_iter=10)
 
-    assert evl.summary['test_score_mean'][('no', 'ols')] == 28.376957869124858
-    assert evl.summary['test_score_mean'][('pr', 'ols')] == 39.007745452637408
+    np.testing.assert_approx_equal(
+            evl.summary['test_score_mean'][('no', 'ols')], 28.376957869124858)
+    np.testing.assert_approx_equal(
+            evl.summary['test_score_mean'][('pr', 'ols')], 39.007745452637408)
 
 
 def test_w_prep_fit():
     """[Model Selection] Test run with preprocessing, single step."""
     evl = Evaluator(rmse, cv=10, shuffle=False, n_jobs=1, random_state=100)
 
-    # Fitting
     evl.fit(X, y,
             estimators=[OLS()],
             param_dicts={'ols': {'offset': randint(1, 10)}},
             preprocessing={'pr': [Scale()], 'no': []},
             n_iter=10)
 
-    assert evl.summary['test_score_mean'][('no', 'ols')] == 28.376957869124858
-    assert evl.summary['test_score_mean'][('pr', 'ols')] == 39.007745452637408
+    np.testing.assert_approx_equal(
+            evl.summary['test_score_mean'][('no', 'ols')], 28.376957869124858)
+    np.testing.assert_approx_equal(
+            evl.summary['test_score_mean'][('pr', 'ols')], 39.007745452637408)
 
 
 def test_w_prep_set_params():
@@ -74,5 +79,7 @@ def test_w_prep_set_params():
             preprocessing={'pr': [Scale()], 'no': []},
             n_iter=10)
 
-    assert evl.summary['test_score_mean'][('no', 'ols')] == 84.856535669630389
-    assert evl.summary['test_score_mean'][('pr', 'ols')] == 142.9297326963281
+    np.testing.assert_approx_equal(
+            evl.summary['test_score_mean'][('no', 'ols')], 84.856535669630389)
+    np.testing.assert_approx_equal(
+            evl.summary['test_score_mean'][('pr', 'ols')], 142.9297326963281)

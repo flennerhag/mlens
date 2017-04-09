b1be019baeb6d8f7846766550919c883469afb0f
Flennerhag <sebastianflennerhag@hotmail.com>
2017-04-09 17:32:32 +0200

    unit testing

--- mlens/preprocessing/tests/test_preprocess.py
+++ mlens/preprocessing/tests/test_preprocess.py
@@ -3,35 +3,33 @@
 :author: Sebastian Flennerhag
 """
 
-from __future__ import division, print_function
-
-from mlens.preprocessing import Subset
 import numpy as np
+from mlens.preprocessing import Subset, Shift
+from mlens.utils.dummy import Data
 
-# training data
-np.random.seed(100)
-X = np.random.random((10, 5))
-
-# noisy output, y = x0 * x1 + x2^2 + x3 - x4^(1/4) + e
-y = X[:, 0] * X[:, 1] + X[:, 2] ** 2 + X[:, 3] - X[:, 4] ** (1 / 4)
-
-# Change scales
-X[:, 0] *= 10
-X[:, 1] += 10
-X[:, 2] *= 5
-X[:, 3] *= 3
-X[:, 4] /= 10
-
+X, _ = Data('stack', False, False).get_data((10, 4), 2)
 
 sub = Subset([0, 1])
 
 def test_subset_1():
-    """[Preprocessing] Subset: assert correct subset."""
+    """[Preprocessing | Subset]: assert correct subset."""
     assert sub.fit_transform(X).shape[1] == 2
 
 
 def test_subset_2():
-    """[Preprocessing] Subset: assert X is returned for empty subset."""
+    """[Preprocessing | Subset]: assert X is returned for empty subset."""
     sub.set_params(**{'subset': None})
     out = sub.fit_transform(X)
     assert id(out) == id(X)
+
+
+def test_shift():
+    """[Preprocessing | Shift] test lagging."""
+    sh = Shift(2)
+
+    sh.fit(X)
+
+    Z = sh.transform(X)
+
+    assert Z.shape[0] + 2 ==  X.shape[0]
+    np.testing.assert_array_equal(Z, X[:-2])

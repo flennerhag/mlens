f4f24b35ed7b0a10f14efb42fe671673e7734589
Flennerhag <sebastianflennerhag@hotmail.com>
2017-07-10 21:38:21 +0200

    bug fix

--- mlens/parallel/tests/test_blend.py
+++ mlens/parallel/tests/test_blend.py
@@ -3,8 +3,6 @@
 Testing ground for parallel backend
 
 """
-import gc
-
 from mlens.utils.dummy import LayerGenerator, Data, Cache
 from mlens.utils.dummy import (layer_fit,
                                layer_predict,
@@ -14,7 +12,8 @@ from mlens.utils.dummy import (layer_fit,
                                lc_from_csv,
                                lc_predict,
                                lc_transform,
-                               lc_feature_prop)
+#                               lc_feature_prop
+                               )
 
 PROBA = False
 PROCESSING = True
@@ -32,8 +31,8 @@ X, y = data.get_data((LEN, WIDTH), MOD)
 
 layer = lg.get_layer('blend', PROBA, PROCESSING)
 lc = lg.get_layer_container('blend', PROBA, PROCESSING)
-lc_p = lg.get_layer_container('blend', PROBA, PROCESSING,
-                              propagate_features=[1])
+#lc_p = lg.get_layer_container('blend', PROBA, PROCESSING,
+#                              propagate_features=[1])
 
 layer.indexer.fit(X)
 
@@ -81,13 +80,11 @@ def test_lc_csv():
     lc_from_csv(lc, cache, X, y, F, wf, P, wp)
 
 
-def test_lc_prop():
-    """[Parallel | Blend | Prep] test layer container feature propagation."""
-    lc_feature_prop(lc_p, X, y, F)
+#def test_lc_prop():
+#    """[Parallel | Blend | Prep] test layer container feature propagation."""
+#    lc_feature_prop(lc_p, X, y, F)
 
 
 def test_close():
     """[Parallel | Blend | Prep] close cache."""
     cache.terminate()
-
-lc_feature_prop(lc_p, X, y, F)

df745c1631d7b20e646678b10022abf2232c90a1
Flennerhag <sebastianflennerhag@hotmail.com>
2017-07-12 17:49:31 +0200

    unit test in-script function

--- mlens/ensemble/tests/test_super_learner.py
+++ mlens/ensemble/tests/test_super_learner.py
@@ -23,6 +23,10 @@ except ImportError:
     run_sklearn = False
 
 
+def in_script_func(y, p):
+    """Test for use of in-script scoring functions."""
+    return np.mean(y - p)
+
 
 FOLDS = 3
 LEN = 6
@@ -43,9 +47,18 @@ ens1 = SuperLearner(folds=FOLDS, scorer=rmse, verbose=100)
 ens1.add(ESTIMATORS, PREPROCESSING, dtype=np.float64)
 ens1.add_meta(OLS(), dtype=np.float64)
 
+ens1_b = SuperLearner(folds=FOLDS, scorer=in_script_func, verbose=100)
+ens1_b.add(ESTIMATORS, PREPROCESSING, dtype=np.float64)
+ens1_b.add_meta(OLS(), dtype=np.float64)
+
 ens2 = SuperLearner(folds=FOLDS, scorer=rmse, verbose=100)
 ens2.add(ECM, dtype=np.float64)
 ens2.add_meta(OLS(), dtype=np.float64)
+
+ens2_b = SuperLearner(folds=FOLDS, scorer=in_script_func, verbose=100)
+ens2_b.add(ECM, dtype=np.float64)
+ens2_b.add_meta(OLS(), dtype=np.float64)
+
 
 if run_sklearn:
     ens3 = SuperLearner(folds=FOLDS, scorer=mean_squared_error)
@@ -58,7 +71,6 @@ def test_run_w_folds():
 
     with open(os.devnull, 'w') as f, redirect_stdout(f):
         ens1.fit(X1, y1)
-
         pred = ens1.predict(X1)
 
     np.testing.assert_array_equal(pred, G1)
@@ -69,7 +81,6 @@ def test_run_wo_folds():
 
     with open(os.devnull, 'w') as f, redirect_stdout(f):
         ens2.fit(X2, y2)
-
         pred = ens2.predict(X2)
 
     np.testing.assert_array_equal(pred, G2)
@@ -121,6 +132,57 @@ def test_scores_wo_folds():
     for k in scores:
 
         assert scores[k] == ens2.scores_['score_mean'][('layer-1', k)]
+
+
+def test_scores_w_folds_in_script():
+    """[SuperLearner] test scoring with folds and in-script scorer."""
+    ens1_b.fit(X1, y1)
+
+    scores = {'no__null': [],
+              'no__offs': [],
+              'sc__offs': []
+              }
+
+    for _, tei in FoldIndex(FOLDS, X1).generate(as_array=True):
+        col = 0
+        for case in sorted(PREPROCESSING):
+            for est_name, __ in ESTIMATORS[case]:
+                s = in_script_func(y1[tei], F1[tei][:, col])
+                scores['%s__%s' % (case, est_name)].append(s)
+
+                col += 1
+
+    for k in scores:
+        scores[k] = np.mean(scores[k])
+
+    for k in scores:
+
+        assert scores[k] == ens1_b.scores_['score_mean'][('layer-1', k)]
+
+
+def test_scores_wo_folds_in_script():
+    """[SuperLearner] test scoring without folds and in-script scorer."""
+    ens2_b.fit(X2, y2)
+    scores = dict()
+    for _, tei in FoldIndex(FOLDS, X2).generate(as_array=True):
+        col = 0
+        for est_name, __ in ECM:
+            s = in_script_func(y2[tei], F2[tei][:, col])
+
+            if not est_name in scores:
+                scores[est_name] = []
+
+            scores[est_name].append(s)
+
+            col += 1
+
+    for k in scores:
+        scores[k] = np.mean(scores[k])
+
+    for k in scores:
+
+        assert scores[k] == ens2_b.scores_['score_mean'][('layer-1', k)]
+
 
 if run_sklearn:
 

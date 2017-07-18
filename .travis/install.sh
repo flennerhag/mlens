c85563c2cf4d4b688f0284ec0bdaf95e9a8a8aea
Flennerhag <sebastianflennerhag@hotmail.com>
2017-07-18 14:02:02 +0200

    travis osx

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,6 +8,7 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get update;
 fi
 
+
 pip install -U coverage coveralls nose-exclude flake8 psutil scipy numpy scikit-learn;
 pip install -r requirements.txt;
 python setup.py install;

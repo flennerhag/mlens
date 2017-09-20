cd792d8cc6f8295f27f1969bdd0c9ac31293755e
Sebastian Flennerhag <sebastianflennerhag@hotmail.com>
2017-09-21 00:11:53 +0100

    include file loader

--- mlens/utils/__init__.py
+++ mlens/utils/__init__.py
@@ -5,16 +5,31 @@
 :licence: MIT
 """
 
-from .utils import pickle_save, pickle_load, print_time, safe_print, CMLog, \
-    kwarg_parser
+from .utils import (pickle_save,
+                    pickle_load,
+                    load,
+                    print_time,
+                    safe_print,
+                    CMLog,
+                    kwarg_parser)
+
 from .formatting import check_instances
 from .validation import check_inputs
 from .checks import (check_is_fitted, check_ensemble_build,
                      assert_correct_format,
                      check_initialized)
 
-__all__ = ['check_inputs', 'check_instances',
-           'check_is_fitted', 'check_ensemble_build',
-           'assert_correct_format', 'check_initialized',
-           'pickle_save', 'pickle_load', 'print_time', 'safe_print', 'CMLog',
-           'kwarg_parser']
+__all__ = ['check_inputs',
+           'check_instances',
+           'check_is_fitted',
+           'check_ensemble_build',
+           'assert_correct_format',
+           'check_initialized',
+           'pickle_save',
+           'pickle_load',
+           'load',
+           'print_time',
+           'safe_print',
+           'CMLog',
+           'kwarg_parser'
+           ]

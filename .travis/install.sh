9cdc544f998b86751f7aa9102bea6c3a840a7ffe
Sebastian Flennerhag <sebastianflennerhag@hotmail.com>
2017-09-27 20:21:24 +0100

    rem upgrade

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,10 +4,7 @@
 set -e
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-
-    # Use apt-get
     sudo apt-get update;
-    sudo apt-get upgrade;
 fi
 
 pip install -U coverage coveralls nose-exclude flake8 psutil scipy numpy scikit-learn;

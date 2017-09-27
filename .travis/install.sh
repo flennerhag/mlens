e81917debd57a4f090ba4aee552957b466682d98
Sebastian Flennerhag <sebastianflennerhag@hotmail.com>
2017-09-27 19:30:31 +0100

    ensure upgrade

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+
 set -e
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 
     # Use apt-get
     sudo apt-get update;
+    sudo apt-get upgrade;
 fi
-
 
 pip install -U coverage coveralls nose-exclude flake8 psutil scipy numpy scikit-learn;
 pip install -r requirements.txt;

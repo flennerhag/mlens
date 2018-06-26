aecd08451550916619cbf6ce83da84ac9e18e9a4
Flennerhag <flenseba@amazon.com>
2018-06-26 11:12:40 +0100

    fetch submodules

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,6 +7,8 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get update;
 fi
 
+git submodule init;
+git submodule update;
 pip install -U coverage coveralls nose-exclude flake8 psutil scipy numpy scikit-learn;
 pip install -r requirements.txt;
 python setup.py install;
